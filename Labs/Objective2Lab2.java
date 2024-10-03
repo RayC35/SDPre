8eb86ee0ce4b1c5818b28b95d474d093891be115
RaymondCostales <raymond.w.costales@gmail.com>
2024-10-03 16:36:33 -0700

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Raymond Costales";
+		int age = 35;
+		String state = "California";
+		System.out.println("Name:" + " " + name + " " + "Age:" + " " + age + " " + "State:" + " " + state);
+	}
+}
