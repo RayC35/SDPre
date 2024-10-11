5aa346d6b3ad69e0bbe853c122ee65c2a65f3d43
RaymondCostales <raymond.w.costales@gmail.com>
2024-10-11 16:58:33 -0700

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,10 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+		int sum = 0;
+
+		for (int control = 0; control <= 20; control += 1) {
+			sum = control + sum;
+		}
+		System.out.println(sum);
+	}
+}
